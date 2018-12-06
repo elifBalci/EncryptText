1a27d5841ba122c88413b3b86578f6ebc7491345
elifBalci <ebalci9949@gmail.com>
2018-12-06 20:58:37 +0300

    bug fixed while generating numbers

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ public class Main {
 
     public static void main(String[] args) {
 	   Value value = new Value();
-	   Encrypt encrypt = new Encrypt(value.a, value.b);
+	   Encrypt encrypt = new Encrypt(value.getA(), value.getB());
 	   encrypt.read();
 	   encrypt.encryptText();
 	   encrypt.write();
--- Value.java
+++ Value.java
@@ -1,9 +1,9 @@
 package com.company;
 
 public class Value {
-    public int alphabetLegth = 26;
-    public int a;
-    public int b;
+    private int alphabetLegth = 26;
+    private int a;
+    private int b;
 
     public Value(){
         a = generate();
@@ -12,11 +12,29 @@ public class Value {
         System.out.println("b is "+ b);
     }
 
-    public int generate(){
+    private int generate(){
         int number = (int)(Math.random()*alphabetLegth);
-        while(number % alphabetLegth != 1 || number == 0)
-            number =  (int)(Math.random()*alphabetLegth);
+        while(gcd(number)>1 || number == 0)
+            number = (int) (Math.random() * alphabetLegth);
 
         return number;
+    }
+
+    private int gcd(int number){
+        int gcd = 1;
+        int min = alphabetLegth;
+        if (alphabetLegth > number)
+            min = number;
+        for(int i = 1; i <= min; ++i) {
+            if(number % i==0 && alphabetLegth % i==0)
+                gcd = i;
+        }
+        return gcd;
+    }
+    public int getA() {
+        return a;
+    }
+    public int getB(){
+        return b;
     }
 }
